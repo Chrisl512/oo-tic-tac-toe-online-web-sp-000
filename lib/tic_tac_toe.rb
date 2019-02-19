bd0879281c8b9e8bc2e8bb5d7f859d8f55237752
Christopher Luna <chris@illusionlayervfx.com>
2019-02-19 03:09:35 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -28,4 +28,90 @@ class TicTacToe
     user_input.to_i - 1
   end
 
+  def move(array, index, value)
+  array[index] = value
+end
+
+def move(array, index, value)
+  array[index] = value
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def valid_move?(board, index)
+ index.between?(0, 8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets
+  index = input_to_index(user_input)
+  if valid_move?(board, index) == true
+   move(board, index, value = current_player(board))
+   display_board(board)
+ else
+     turn(board)
+   end
+ end
+
+ def turn_count(board)
+  counter = 0
+  board.each do |turn|
+    if turn == "X" || turn == "O"
+      counter += 1
+    end
+  end
+   counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  else
+    "O"
+  end
+ end
+
+ def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+  if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
+    return win_combination
+  elsif board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
+    return win_combination
+  end
+ end
+ false
+end
+
+def full?(board)
+  board.each do |full|
+    if full == " "
+     return false
+    end
+  end
+end
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+def over?(board)
+    if draw?(board)
+     true
+   elsif !!won?(board)
+     true
+ end
+end
+
+def winner(board)
+if won?(board)
+  return board[won?(board)[0]]
+else
+  return nil
+ end
+end
+
+
 end
