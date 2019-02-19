f939a149e6f2c6abe366fd539787caf09c9b1a90
Christopher Luna <chris@illusionlayervfx.com>
2019-02-19 04:26:02 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -67,9 +67,9 @@ def current_player
 
  def won?
   WIN_COMBINATIONS.each do |win_combination|
-  if win_combination[0] == "X" && win_combination[1] == "X" && win_combination[2] == "X"
+  if @board[win_combination[0]] == "X" && @board[win_combination[1]] == "X" && @board[win_combination[2]] == "X"
     return win_combination
-  elsif win_combination[0] == "O" && win_combination[1] == "O" && win_combination[2] == "O"
+  elsif @board[win_combination[0]] == "O" && @board[win_combination[1]] == "O" && @board[win_combination[2]] == "O"
     return win_combination
   end
  end
