8165e26faab88514dca83434a7acb4f6ef636870
Christopher Luna <chris@illusionlayervfx.com>
2019-02-19 03:00:12 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,9 +1,11 @@
 board = Array.new(9, " ")
 
 class TicTacToe
-  def initialize(board)
-    @board = board
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
   end
+
+  def board=(board)
 
   WIN_COMBINATIONS = [
   [0,1,2],
